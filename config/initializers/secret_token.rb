d70fc5eaa20f1170006cbbe9c9f3bb1c518906bc
Isaac Elias <nicecurelom@gmail.com>
2013-06-19 20:06:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapSandbox::Application.config.secret_token = '78bd5e27927996621222289b67b78997d0546a97783fbdb2c17af80c0dbe9aa3a8257a611d2028e3fcc9c1cf660ea12b61220c5b6f0a74cf55e23c06a5000120'
